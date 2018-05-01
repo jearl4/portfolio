c2f5c12c0d1ea69af57db47f9ee160ae2a8b7e37
jearl4 <jtearl188@gmail.com>
2018-04-30 21:19:39 -0400

    Set up routing for about, home, and 404. Nav links dont work, no tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: LandingPageComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+// only need to specify new route here
+export const routingComponents = [LandingPageComponent, AboutComponent, PageNotFoundComponent];
