0facb3434f57e551f53522dd452e1d0470f680b2
jearl4 <jtearl188@gmail.com>
2018-05-01 21:31:47 -0400

    added angular material card module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule,  MatIconModule} from '@angular/material';
+import { MatButtonModule, MatToolbarModule,  MatIconModule, MatCardModule} from '@angular/material';
 @NgModule({
-    imports: [MatButtonModule, MatToolbarModule, MatIconModule],
-    exports: [MatButtonModule, MatToolbarModule, MatIconModule]
+    imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
+    exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule]
 })
 
 export class MaterialModule { }
