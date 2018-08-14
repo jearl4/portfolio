ffb217a4fd7d14d82102edf13f1ce9237503ade4
jearl4 <jtearl188@gmail.com>
2018-08-13 22:27:55 -0400

    Fixed onSubmit error failing build

--- src/app/capm-form/capm-form.component.ts
+++ src/app/capm-form/capm-form.component.ts
@@ -9,13 +9,16 @@ import { Capm } from '../capm/capm';
 })
 export class CapmFormComponent implements OnInit {
 
-  model = new Capm(0, 2.064, .56, 6.0);
+  model = new Capm(0, 2.064, .50, 6.0);
 
   submitted = false;
 
   newCapm() { this.model = new Capm(0, 0, 0, 0); }
 
-  onsubmit() { this.submitted = true; }
+  onSubmit() {
+    this.submitted = true;
+    //this.model.rateOfReturn = this.model.riskFreeRate + this.model.beta * (this.model.marketRate - this.model.riskFreeRate);
+  }
 
   // clean up after dev
   get diagnostic() { return JSON.stringify(this.model); }
