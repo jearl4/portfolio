fda72fbd9d5bd91b77c856629dc97d93427c88f2
jearl4 <jtearl188@gmail.com>
2018-08-13 23:29:48 -0400

    exposing firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCrCLTW-SxTGNIZRBwO6wfoP-8mCMwpa1Q',
+    authDomain: 'portfolio-51dd4.firebaseapp.com',
+    databaseURL: 'https://portfolio-51dd4.firebaseio.com',
+    projectId: 'portfolio-51dd4',
+    storageBucket: 'portfolio-51dd4.appspot.com',
+    messagingSenderId: '120115284338'
+  }
 };
