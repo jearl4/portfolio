a66ce9cc9060d3540a792c44a63e0ead963b5bf7
jearl4 <jtearl188@gmail.com>
2018-08-13 22:56:29 -0400

    Hide capm form on submit. Display capm formula on submit. Added left margin to new capm button

--- src/app/capm-form/capm-form.component.ts
+++ src/app/capm-form/capm-form.component.ts
@@ -9,20 +9,16 @@ import { Capm } from '../capm/capm';
 })
 export class CapmFormComponent implements OnInit {
 
-  model = new Capm(0, 2.064, .50, 6.0);
+  model = new Capm(1, 2.064, .50, 6.0);
 
   submitted = false;
 
   newCapm() { this.model = new Capm(0, 0, 0, 0); }
 
   onSubmit() {
-    this.submitted = true;
-    //this.model.rateOfReturn = this.model.riskFreeRate + this.model.beta * (this.model.marketRate - this.model.riskFreeRate);
+  this.submitted = true;
+    this.model.rateOfReturn = this.model.riskFreeRate + this.model.beta * (this.model.marketRate - this.model.riskFreeRate);
   }
-
-  // clean up after dev
-  get diagnostic() { return JSON.stringify(this.model); }
-
   constructor() { }
 
   ngOnInit() {
