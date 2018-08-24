688176f98674dd3361e52c01c98834b109abe04c
jearl4 <jtearl188@gmail.com>
2018-08-23 22:55:40 -0400

    creating config object for contentful

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  // color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -8,6 +14,13 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
+
+  tiles: Tile[] = [
+    {
+      text: 'TEST1', cols: 1, rows: 1},
+    { text: 'TEST2', cols: 1, rows: 1 },
+    { text: 'TEST3', cols: 1, rows: 1 }
+  ];
 
   ngOnInit() {
   }
