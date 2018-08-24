b02404023b9c9bdc7b30f5784dc62d529abf87e8
jearl4 <jtearl188@gmail.com>
2018-08-24 00:23:08 -0400

    Added test to check default values. Tests to check new model creationa re failing

--- src/app/capm-form/capm-form.component.spec.ts
+++ src/app/capm-form/capm-form.component.spec.ts
@@ -28,4 +28,40 @@ describe('CapmFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('When form is created, it ', () => {
+    it('should have the default beta value ', () => {
+      expect(component.model.beta).toBe(.5);
+    });
+
+    it('should have the default risk free rate ', () => {
+      expect(component.model.riskFreeRate).toBe(2.064);
+    });
+
+    it('should have the default market return', () => {
+      expect(component.model.marketRate).toBe(6);
+    });
+  });
+
+  describe('When a new capm model is created, it ', () => {
+    beforeEach(() => {
+      spyOn(component, 'newCapm').and.returnValues({
+        beta: 0, riskFreeRate: 0,
+        marketRate: 0
+      });
+      component.newCapm();
+    });
+
+    it('should have a zero beta value ', () => {
+      expect(fixture.componentInstance.model.beta).toBe(0);
+    });
+
+    it('should have a zero risk free rate ', () => {
+      expect(fixture.componentInstance.model.riskFreeRate).toBe(0);
+    });
+
+    it('should have a zero market return', () => {
+      expect(fixture.componentInstance.model.marketRate).toBe(0);
+    });
+  });
 });
