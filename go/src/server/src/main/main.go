3ae83149a03b8492b69f84c8e16e12d5f17ab039
jearl4 <jtearl188@gmail.com>
2018-11-20 21:45:04 -0500

    Trying to add stock revenue to firebase db. Currently adds empty Stock object

--- go/src/server/src/main/main.go
+++ go/src/server/src/main/main.go
@@ -1,43 +1,79 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"util"
 
+	firebase "firebase.google.com/go"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"google.golang.org/api/option"
 )
 
 func main() {
+	sa := option.WithCredentialsFile("C:/Users/JT/Desktop/Angular/Angular5/portfolio/portfolio-51dd4-firebase-adminsdk-57as6-28b4528ea5.json")
+	app, err := firebase.NewApp(context.Background(), nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	stock := getStock()
+	log.Print(stock)
+
+	// result, err := client.Collection("sampleData").Doc("stock").Set(context.Background(), map[string]interface{})
+	_, _, err = client.Collection("stock").Add(context.Background(), map[string]interface{}{
+		"blob": stock,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Print("end call")
+
+	defer client.Close()
+
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello-world", helloWorld)
-
-	http.Handle("/", r)
+	// Solves Cross Origin Access Issue
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:4200"},
+	})
+	handler := c.Handler(r)
 
 	srv := &http.Server{
-		Handler: r,
+		Handler: handler,
 		Addr:    ":" + os.Getenv("PORT"),
 	}
 
 	log.Fatal(srv.ListenAndServe())
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	var data = struct {
-		Title string `json:"title"`
-	}{
-		Title: "Golang + Angular Starter Kit",
-	}
-
-	jsonBytes, err := util.StructToJSON(data)
+func getStock() Stock {
+	var appl Stock
+	response, err := http.Get("https://financialmodelingprep.com/api/financials/income-statement/AAPL")
 	if err != nil {
-		fmt.Print(err)
+		fmt.Printf("the request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		// fmt.Println(string(data))
+		err = json.Unmarshal(data, &appl)
+		fmt.Println(appl.Revenue)
 	}
+	return appl
+}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonBytes)
-	return
+type Stock struct {
+	Revenue string `json:"Revenue"`
 }
